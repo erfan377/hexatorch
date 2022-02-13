06114339be4653bc388a8c2258e51447470e9d78
erfan377 <erfan377@gmail.com>
2022-02-13 13:44:27 -0800

    added button to page

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,7 +1,9 @@
+
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import  './popup.css'
 import logo from "./logo.jpg";
+import Button from 'react-bootstrap/Button';
 
 // const Ps = {
 //   background-color: 'black',
@@ -24,50 +26,59 @@ class NameForm extends React.Component<{}, { value: string }> {
   }
 
   handleSubmit(event) {
+    console.log('erfan was here');
     alert('A name was submitted: ' + this.state.value); //error here
     event.preventDefault();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          <input
-            type="text"
-            placeholder="Type an address and press enter..."
-            value={this.state.value}
-            onChange={this.handleChange}
-            style={{
-              padding: "10px 20px",
-              width: "300px",
-              textAlign: "left",
-              // align: "center",
-              border: "0px",
-              marginLeft: "25px",
-              backgroundColor: '#EDE7E7',
-              borderRadius: '10px',
-            }}
-          />
-        </label>
-      </form>
+      <div className='body'>
+        <div className='header'>
+          <text className = 'gaspricetitle'> ETH Mid Gas Price: </text>
+          <br />
+          <text className = 'gasprice'>  73 Gwei, $3.84 USD </text>
+        </div>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <input
+              type="text"
+              placeholder="Type an address and press enter..."
+              value={this.state.value}
+              onChange={this.handleChange}
+              style={{
+                padding: "10px 20px",
+                width: "300px",
+                textAlign: "left",
+                // align: "center",
+                border: "0px",
+                marginLeft: "25px",
+                backgroundColor: '#EDE7E7',
+                borderRadius: '10px',
+              }}
+            />
+          </label>
+          <img className = 'logo' src = {logo}/>
+          <Button variant="primary" type="submit"> Safe List</Button>
+        </form>
+    </div>
     );
   }
 }
 
+function sendForm(){
+  //check for validity of link
+}
+
+
 const App: React.FC<{}> = () => {
   return (
-    <div className='body'>
-      <div className='header'>
-      <text className = 'gaspricetitle'> ETH Mid Gas Price: </text>
-      <br />
-      <text className = 'gasprice'>  73 Gwei, $3.84 USD </text>
-      </div>
       <NameForm/>
-      <img className = 'logo' src = {logo}/>
-    </div>
   )
 }
 
 const root = document.createElement('div')
 document.body.appendChild(root)
 ReactDOM.render(<App />, root)
+
